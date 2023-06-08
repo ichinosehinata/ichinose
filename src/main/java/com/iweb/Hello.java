40afaac49e27377bc51f8fe94cabc0d4f10a995d
ichinose <2667035910@qq.com>
2023-06-08 19:17:44 +0800

    test

---
+++ src/main/java/com/iweb/Hello.java
@@ -0,0 +1,17 @@
+package com.iweb;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 26670
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hello {
+    private String name;
+    private int age;
+}
---
+++ src/main/java/com/iweb/Test.java
@@ -0,0 +1,8 @@
+package com.iweb;
+
+/**
+ * @author ichinose
+ * @date 2023/6/8 16:21
+ */
+public class Test {
+}
